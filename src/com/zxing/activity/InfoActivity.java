7c413ee58a3ed4c1e22c28a106359324e93e56e4
coco0302 <lvyuan1993@gmail.com>
2016-01-11 09:46:43 +0100

    latest version 11/01/2016

---
+++ src/com/zxing/activity/InfoActivity.java
@@ -0,0 +1,14 @@
+package com.zxing.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ericssonlabs.R;
+
+
+public class InfoActivity extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.information);
+	}
+}
